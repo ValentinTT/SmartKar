f7059fae1c06bdfcc0391febd024d7c5573978fc
V-TT <valentintapiatorti@gmail.com>
2017-02-01 16:46:50 -0300

    ChooserDeviceActivity UI done. First implementation of the ChooserDeviceActivity functions

--- app/src/main/java/com/vtt/smartkar/ChooseDeviceActivity.java
+++ app/src/main/java/com/vtt/smartkar/ChooseDeviceActivity.java
@@ -1,26 +1,86 @@
 package com.vtt.smartkar;
 
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindColor;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ChooseDeviceActivity extends AppCompatActivity{
     private static final String DEBUG_TAG = "ChooseDeviceActivity";
+    private static final int REQUEST_CODE_ACTIVATE_BLUETOOTH = 1;
 
-    @OnClick(R.id.connect_button)
-    public void tryConnection(){
-        Log.i(DEBUG_TAG, "Connection button pressed");
-    }
-
+    private BluetoothAdapter mBluetoothAdapter;
+    @BindView(R.id.connect_button)
+    protected Button mConnectButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_device);
         ButterKnife.bind(this);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) { //This means the mobile doesn't have bluetooth.
+            mConnectButton.setEnabled(false);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("No bluetooth");
+            builder.setMessage("Your device doesn't have bluetooth");
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tryConnection();
+            }
+        });
+    }
+
+    private void tryConnection(){
+        if(!mBluetoothAdapter.isEnabled()){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Activate bluetooth");
+            builder.setMessage("Bluetooth is disabled. Do you want to enable it?");
+            builder.setNegativeButton("No", null);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent BluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(BluetoothIntent, REQUEST_CODE_ACTIVATE_BLUETOOTH);
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            showPosibleConnections();
+        }
+    }
+
+    private void showPosibleConnections(){
+        Log.i(DEBUG_TAG, "Connection button pressed");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_ACTIVATE_BLUETOOTH) { //Intent that activate bluetooth
+            if(resultCode == RESULT_OK) { //Bluetooth is now activated
+                showPosibleConnections();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
